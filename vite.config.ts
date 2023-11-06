814ca5b5f5f6442fca29bdef3da7b6cde16d2f71
Victor <victorandreimaciel67@gmail.com>
2023-11-06 12:30:26 -0300

    Git pages tentativa de fix denovo

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/RPG-POO",
+  base: "/RPG-POO/",
+  build: {
+    outDir: 'dist'
+  },
 })
