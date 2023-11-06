33ef713f294a7d0681f05f97c8614e5883291c52
Victor <victorandreimaciel67@gmail.com>
2023-11-06 02:27:25 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { menu } from './components/Menu'
+
+function App() {
+
+  return (
+    <div>
+      {menu()}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Menu.tsx
@@ -0,0 +1,12 @@
+import './Menu.css';
+export function menu() {
+    return (
+    <div className='menu'>
+        <div><img src="./logo.png" alt="logo" height={450} width={300} /></div>
+        <div className='botao'><p>Entrar na Dungeon</p></div>
+        <div className='botao'><p>Criar Personagem</p></div>
+        <div className='botao'><p>Abrir Personagem</p></div>
+        <div className='botao'><p>Tutorial</p></div>
+    </div>
+    )
+  }
