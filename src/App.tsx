630f48ec5d7781f3cbd308e81648db4d3a702491
Victor <victorandreimaciel67@gmail.com>
2023-11-06 17:03:53 -0300

    Mudancas ao personagem e Testes em console.logs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import './App.css'
-import  menu from './components/Menu.tsx'
+import { Atributos, Personagem } from './classes/Personagem.tsx';
+import menu from './components/Menu.tsx'
 
 function App() {
+
+  const atributos = new Atributos(12, 16, 14, 10, 13, 15); // Substitua esses valores pelos atributos desejados
+  atributos.percepcao = true
+  const personagem = new Personagem("Nome do Personagem", 30, 30, atributos);
+
+  console.log("Personagem Criado:");
+  console.log("Nome:", personagem.nome);
+  console.log("Pontos de Vida:", personagem.pontosVida);
+  console.log("Pontos de Vida Máximos:", personagem.pontosVidaMaximos);
+  console.log(
+    `Forca: ${personagem.atributos.forca} (${personagem.atributos.forcaBonus >= 0 ? '+' : ''}${personagem.atributos.forcaBonus}),
+    Destreza: ${personagem.atributos.destreza} (${personagem.atributos.destrezaBonus >= 0 ? '+' : ''}${personagem.atributos.destrezaBonus}),
+    Constituicao: ${personagem.atributos.constituicao} (${personagem.atributos.constituicaoBonus >= 0 ? '+' : ''}${personagem.atributos.constituicaoBonus}),
+    Inteligencia: ${personagem.atributos.inteligencia} (${personagem.atributos.inteligenciaBonus >= 0 ? '+' : ''}${personagem.atributos.inteligenciaBonus}),
+    Sabedoria: ${personagem.atributos.sabedoria} (${personagem.atributos.sabedoriaBonus >= 0 ? '+' : ''}${personagem.atributos.sabedoriaBonus}),
+    Carisma: ${personagem.atributos.carisma} (${personagem.atributos.carismaBonus >= 0 ? '+' : ''}${personagem.atributos.carismaBonus})`
+  );
+  console.log("Percepção Passiva:", personagem.percepcaoPassiva);
+  console.log("Classe de Armadura:", personagem.classeArmadura);
 
   return (
     <div>
--- src/classes/personagem.tsx
+++ src/classes/personagem.tsx
@@ -1,43 +1,125 @@
-class Atributos {
+export class Atributos {
     forca: number;
     destreza: number;
     constituicao: number;
     inteligencia: number;
     sabedoria: number;
     carisma: number;
-  
+
+    // Bônus dos Atributos
+    forcaBonus: number;
+    destrezaBonus: number;
+    constituicaoBonus: number;
+    inteligenciaBonus: number;
+    sabedoriaBonus: number;
+    carismaBonus: number;
+
+    // Proficiências em Testes de Resistência
+    testeForca: boolean;
+    testeDestreza: boolean;
+    testeConstituicao: boolean;
+    testeInteligencia: boolean;
+    testeSabedoria: boolean;
+    testeCarisma: boolean;
+
+    // Proficiencias em Perícias
+    atletismo: boolean;
+    sobrevivencia: boolean;
+    maoHabil: boolean;
+    furtividade: boolean;
+    acrobacias: boolean;
+    arcanismo: boolean;
+    natureza: boolean;
+    medicina: boolean;
+    percepcao: boolean;
+    historia: boolean;
+    intuicao: boolean;
+    persuasao: boolean;
+    enganacao: boolean;
+    intimidacao: boolean;
+
+    // Bônus de Proficiência
+    bonusProficiencia: number;
+
     constructor(
-      forca: number,
-      destreza: number,
-      constituicao: number,
-      inteligencia: number,
-      sabedoria: number,
-      carisma: number
+        forca: number,
+        destreza: number,
+        constituicao: number,
+        inteligencia: number,
+        sabedoria: number,
+        carisma: number
     ) {
-      this.forca = forca;
-      this.destreza = destreza;
-      this.constituicao = constituicao;
-      this.inteligencia = inteligencia;
-      this.sabedoria = sabedoria;
-      this.carisma = carisma;
+        this.forca = forca;
+        this.destreza = destreza;
+        this.constituicao = constituicao;
+        this.inteligencia = inteligencia;
+        this.sabedoria = sabedoria;
+        this.carisma = carisma;
+
+        // Calcula os bônus dos atributos (+1 a cada 2 numeros acima de 10, -1 a cada 2 numeros abaixo de 10)
+        this.forcaBonus = Math.floor((this.forca - 10) / 2);
+        this.destrezaBonus = Math.floor((this.destreza - 10) / 2);
+        this.constituicaoBonus = Math.floor((this.constituicao - 10) / 2);
+        this.inteligenciaBonus = Math.floor((this.inteligencia - 10) / 2);
+        this.sabedoriaBonus = Math.floor((this.sabedoria - 10) / 2);
+        this.carismaBonus = Math.floor((this.carisma - 10) / 2);
+
+        // Inicializa as proficiências em testes de resistência e perícias
+        this.testeForca = false;
+        this.testeDestreza = false;
+        this.testeConstituicao = false;
+        this.testeInteligencia = false;
+        this.testeSabedoria = false;
+        this.testeCarisma = false;
+
+        this.atletismo = false;
+        this.sobrevivencia = false;
+        this.maoHabil = false;
+        this.furtividade = false;
+        this.acrobacias = false;
+        this.arcanismo = false;
+        this.natureza = false;
+        this.medicina = false;
+        this.percepcao = false;
+        this.historia = false;
+        this.intuicao = false;
+        this.persuasao = false;
+        this.enganacao = false;
+        this.intimidacao = false;
+
+        // Inicializa o bônus de proficiência
+        this.bonusProficiencia = 2; // TODO: Bonus de proficiencia baseado em nivel do personagem
     }
-  }
-  
-  class Personagem {
+}
+
+export class Personagem {
     nome: string;
     pontosVida: number;
     pontosVidaMaximos: number;
     atributos: Atributos;
-  
+
+    // Percepção Passiva e Classe de Armadura (CA)
+    percepcaoPassiva: number;
+    classeArmadura: number;
+
     constructor(
-      nome: string,
-      pontosVida: number,
-      pontosVidaMaximos: number,
-      atributos: Atributos
+        nome: string,
+        pontosVida: number,
+        pontosVidaMaximos: number,
+        atributos: Atributos
     ) {
-      this.nome = nome;
-      this.pontosVida = pontosVida;
-      this.pontosVidaMaximos = pontosVidaMaximos;
-      this.atributos = atributos;
+        this.nome = nome;
+        this.pontosVida = pontosVida;
+        this.pontosVidaMaximos = pontosVidaMaximos;
+        this.atributos = atributos;
+
+        // Calcula a Percepcao Passiva (PP)
+        this.percepcaoPassiva = 10 + this.atributos.sabedoriaBonus;
+        if (this.atributos.percepcao) {
+            this.percepcaoPassiva += this.atributos.bonusProficiencia;
+        }
+
+        // Calcula a classe de armadura (CA)
+        this.classeArmadura = 10 + this.atributos.destrezaBonus;
     }
-  }
+}
