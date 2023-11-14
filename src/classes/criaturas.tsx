abe86a0d7028a43ac48674a6bfc1da272010740a
Vix <victorandreimaciel67@gmail.com>
2023-11-14 11:13:14 -0300

    Logica de inimigos Unicos

--- src/classes/criaturas.tsx
+++ src/classes/criaturas.tsx
@@ -1,7 +1,16 @@
 import * as Equipamentos from "./equipamentos";
 import { Atributos, Personagem } from "./personagem";
 
-const atributos = new Atributos(8, 14, 10, 10, 8, 8);
-export const goblin1 = new Personagem("Goblin", 7, 7, atributos, Equipamentos.adaga, Equipamentos.couro, Equipamentos.vazioArma);
-goblin1.atributos.furtividade = true;
-goblin1.calcularClasseArmadura();
+let contadorGoblin = 1;
+
+export function criarNovoGoblin(): Personagem {
+  const atributos = new Atributos(8, 14, 10, 10, 8, 8);
+  const nome = `Goblin ${contadorGoblin}`;
+  contadorGoblin++;
+
+  const novoGoblin = new Personagem(nome, 7, 7, atributos, Equipamentos.adaga, Equipamentos.couro, Equipamentos.vazioArma);
+  novoGoblin.atributos.furtividade = true;
+  novoGoblin.calcularClasseArmadura();
+
+  return novoGoblin;
+}
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,11 +1,12 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import 'react-responsive-modal/styles.css';
 import { Modal } from 'react-responsive-modal';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import * as Equipamentos from '../classes/equipamentos';
 import './Menu.css';
 import { Personagem } from '../classes/personagem';
 import { combate } from '../classes/combate';
-import { goblin1 } from '../classes/criaturas';
+import { criarNovoGoblin } from '../classes/criaturas';
 
 
 
@@ -67,14 +68,14 @@ function menu({ personagem, atualizarPersonagem }: MenuProps) {
             <div className='botaoDebug' onClick={() => escolherArma(Equipamentos.vazioArma)}><p>Largar Arma</p></div>
             <div className='botaoDebug' onClick={() => escolherArmadura(Equipamentos.vazioArmadura)}><p>Desequipar Armadura</p></div>
             <div className='botaoDebug' onClick={() => escolherEquip(Equipamentos.vazioArma)}><p>Largar Equip Secundario</p></div>
-            <div className='botaoDebug' onClick={() => { alvoSelecionado && combate.ataque(personagem, alvoSelecionado); setAtt(att + 1);}}><p>Atacar</p></div>
+            <div className='botaoDebug' onClick={() => { alvoSelecionado && combate.ataque(personagem, alvoSelecionado); setAtt(att + 1); }}><p>Atacar</p></div>
             <div className='textDebug'><p>Selecione um alvo:</p></div>
             {combate.participantes.map((participante, index) => (<div key={index} className='botaoDebug' onClick={() => selecionarAlvo(participante)}><p>Selecionar {participante.nome}</p></div>))}
           </div>
           <div>
             <div className='textDebug'><p>Debug de Criaturas</p></div>
-            <div className='botaoDebug' onClick={() => { combate.adicionarParticipante(goblin1); setAtt(att + 1);}}><p>Adicionar 1 Goblin</p></div>
-            <div className='botaoDebug' onClick={() => { combate.iniciarCombate(); setAtt(att + 1);}}><p>Entrar em combate</p></div>
+            <div className='botaoDebug' onClick={() => { combate.adicionarParticipante(criarNovoGoblin()); setAtt(att + 1); }}><p>Adicionar 1 Goblin</p></div>
+            <div className='botaoDebug' onClick={() => { combate.iniciarCombate(); setAtt(att + 1); }}><p>Entrar em combate</p></div>
             <div className='botaoDebug' onClick={() => { combate.iniciarRodada(); setAtt(att + 1); }}><p>Passar Turno</p></div>
           </div>
         </div>
